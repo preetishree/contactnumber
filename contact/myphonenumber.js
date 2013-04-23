9aba902f81aa39eb0d6db51db7dc7023b58d8773
preetishree <preetishree@krititech.in>
2013-04-23 19:15:00 +0530

    Update myphonenumber.js

--- contact/myphonenumber.js
+++ contact/myphonenumber.js
@@ -2,8 +2,8 @@
 var MyPhoneNumberPlugin = {
 	getMyPhoneNumber: function(onSuccess, onError) {
 		console.log("at getMyPhoneNumber");
-		//return PhoneGap.exec(onSuccess, onError, "MyPhoneNumberPlugin", "getMyPhoneNumber", []);
-		return cordova.exec("MyPhoneNumberPlugin.getMyPhoneNumber", GetFunctionName(onSuccess), GetFunctionName(onError));
+		return PhoneGap.exec(onSuccess, onError, "MyPhoneNumberPlugin", "getMyPhoneNumber", []);
+		//return cordova.exec("MyPhoneNumberPlugin.getMyPhoneNumber", GetFunctionName(onSuccess), GetFunctionName(onError));
 	}
 }
 
